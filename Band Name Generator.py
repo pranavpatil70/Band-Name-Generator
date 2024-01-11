5ee339a6a843396232627c0db03459b1dcb948d7
pranavpatil70 <pvp3400@gmail.com>
2024-01-11 22:39:35 +0530

    code added to program

---
+++ Band Name Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to band name generator!!!")
+city = input("which city did you grow in?\n")
+pet = input("what is the name of your cat?\n")
+
+print("Your band name can be "+city+' '+pet+'!')
